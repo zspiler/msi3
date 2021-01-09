f689fa25936032ab850ff36aaf47812ec4304111
Ubuntu <zanspiler1@gmail.com>
2021-01-09 22:51:00 +0100

    Added cache service and deployment, rebuilt image

--- src/app.py
+++ src/app.py
@@ -1,26 +1,26 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
 from flask_caching import Cache
+import socket
 
 app = Flask(__name__)
-# db = MongoClient("mongodb://mongo-svc.default.svc.cluster.local:27017").test_db
+db = MongoClient("mongodb://mongo-svc.default.svc.cluster.local:27017").test_db
 
+cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://redis-svc.default.svc.cluster.local:6379/0'})
+hostname = socket.gethostname()
 
-# cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://redis:6379/0'})
-
-# counter = db.col.find_one()
-# if counter == None:
-    # db.col.insert_one({ 'counter' : 1 })
+counter = db.col.find_one()
+if counter == None:
+    db.col.insert_one({ 'counter' : 1 })
 
 @app.route('/', methods=['GET', 'POST'])
-# @cache.cached(timeout=5)
+@cache.cached(timeout=5)
 def main():
-    # counter = db.col.find_one()['counter'] 
-    # if request.method == 'POST':
-        # counter += 1
-        # db.col.find_one_and_update({}, {'$set': { 'counter': counter }})
-    # return render_template('index.html', x=counter) 
-    return 'yes'
+    counter = db.col.find_one()['counter'] 
+    counter += 1
+    db.col.find_one_and_update({}, {'$set': { 'counter': counter }})        
+    return render_template('index.html', x=counter, host=hostname) 
+
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
