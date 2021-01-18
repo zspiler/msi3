981d91e11471bbe4bc366c6255819aed33914fb6
Ubuntu <zanspiler1@gmail.com>
2021-01-18 21:51:34 +0100

    Minor fixes

--- src/app.py
+++ src/app.py
@@ -38,7 +38,7 @@ def liveness():
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
-        return f"pod: {hostname}, version: v2\n"
+        return f"pod: {hostname}, version: v1\n"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
